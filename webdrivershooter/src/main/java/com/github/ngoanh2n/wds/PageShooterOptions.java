0a21ea0feddc08ae0e6b6d82ee5d44dcfb6761bd
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 23:00:13 +0700

    Implement PageShooterOptions

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/PageShooterOptions.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/PageShooterOptions.java
@@ -1,4 +1,34 @@
 package com.github.ngoanh2n.wds;
 
-public class PageShooterOptions {
+public interface PageShooterOptions extends ShooterOptions {
+    static Builder builder() {
+        return new Builder();
+    }
+
+    static PageShooterOptions defaults() {
+        return builder().build();
+    }
+
+    //===============================================================================//
+
+    final class Builder extends ShooterOptions.Builder<Builder> {
+        public PageShooterOptions build() {
+            return new PageShooterOptions() {
+                @Override
+                public int delayDurationForScrolling() {
+                    return delayDurationForScrolling;
+                }
+
+                @Override
+                public boolean checkDevicePixelRatio() {
+                    return checkDevicePixelRatio;
+                }
+
+                @Override
+                public int shooterStrategy() {
+                    return shooterStrategy;
+                }
+            };
+        }
+    }
 }
