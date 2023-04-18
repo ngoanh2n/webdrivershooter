6a066fc7ba9d88c6b5453daf7fce4424c7bd978f
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 22:57:10 +0700

    Add constructor for Browser

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Browser.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Browser.java
@@ -1,4 +1,13 @@
 package com.github.ngoanh2n.wds;
 
+import org.openqa.selenium.WebDriver;
+
 public class Browser {
+    private final WebDriver driver;
+    private final Double dpr;
+
+    public Browser(WebDriver driver, ShooterOptions options) {
+        this.driver = driver;
+        this.dpr = 1.0;
+    }
 }
