e271965c7bd9eadf0a1fe937463d26310812544c
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 22:44:40 +0700

    Implement Screenshot

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screenshot.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screenshot.java
@@ -1,4 +1,11 @@
 package com.github.ngoanh2n.wds;
 
+import java.awt.image.BufferedImage;
+
 public class Screenshot {
+    private BufferedImage image;
+
+    public Screenshot(BufferedImage image) {
+        this.image = image;
+    }
 }
