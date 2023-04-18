089586667f7760d7e02b0173a2fd971d9fe06bf9
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 22:44:04 +0700

    Implement ShooterOptions

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterOptions.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterOptions.java
@@ -1,4 +1,73 @@
 package com.github.ngoanh2n.wds;
 
-public class ShooterOptions {
+public interface ShooterOptions {
+    int shooterStrategy();
+
+    boolean checkDevicePixelRatio();
+
+    int delayDurationForScrolling();
+
+    //-------------------------------------------------------------------------------//
+
+    @SuppressWarnings("unchecked")
+    class Builder<T extends Builder<?>> {
+        protected int shooterStrategy;
+        protected boolean checkDevicePixelRatio;
+        protected int delayDurationForScrolling;
+
+        protected Builder() {
+            this.shooterStrategy = 2;
+            this.checkDevicePixelRatio = true;
+            this.delayDurationForScrolling = 100;
+        }
+
+        public T viewport() {
+            this.shooterStrategy = 1;
+            return (T) this;
+        }
+
+        public T fullScroll() {
+            this.shooterStrategy = 2;
+            return (T) this;
+        }
+
+        public T verticalScroll() {
+            this.shooterStrategy = 3;
+            return (T) this;
+        }
+
+        public T horizontalScroll() {
+            this.shooterStrategy = 4;
+            return (T) this;
+        }
+
+        public T checkDevicePixelRatio(boolean enabled) {
+            this.checkDevicePixelRatio = enabled;
+            return (T) this;
+        }
+
+        public T setDelayDurationForScrolling(int value) {
+            this.delayDurationForScrolling = value;
+            return (T) this;
+        }
+
+        public ShooterOptions build() {
+            return new ShooterOptions() {
+                @Override
+                public int shooterStrategy() {
+                    return shooterStrategy;
+                }
+
+                @Override
+                public int delayDurationForScrolling() {
+                    return delayDurationForScrolling;
+                }
+
+                @Override
+                public boolean checkDevicePixelRatio() {
+                    return checkDevicePixelRatio;
+                }
+            };
+        }
+    }
 }
