fef132c3f7c819d27868516984376319f6d12bcf
ngoanh2n <ngoanh2n@gmail.com>
2023-04-20 22:06:08 +0700

    Add JS script: com/github/ngoanh2n/wds/GetDocumentHeight.js

---
+++ webdrivershooter/src/main/resources/com/github/ngoanh2n/wds/GetDocumentHeight.js
@@ -0,0 +1,7 @@
+return Math.max(
+  document.body.scrollHeight,
+  document.body.offsetHeight,
+  document.documentElement.clientHeight,
+  document.documentElement.scrollHeight,
+  document.documentElement.offsetHeight
+);
