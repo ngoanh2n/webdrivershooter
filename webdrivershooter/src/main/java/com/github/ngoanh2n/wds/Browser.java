7787c8e9dfffaf37e8aea04b991e7acecb2b2bda
ngoanh2n <ngoanh2n@gmail.com>
2023-04-20 21:57:46 +0700

    Add Browser.executeScript()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Browser.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Browser.java
@@ -1,5 +1,7 @@
 package com.github.ngoanh2n.wds;
 
+import com.github.ngoanh2n.Resource;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class Browser {
@@ -9,5 +11,11 @@ public class Browser {
     public Browser(WebDriver driver, ShooterOptions options) {
         this.driver = driver;
         this.dpr = 1.0;
+    }
+
+    public Object executeScript(String resourceName, Object... args) {
+        String script = Resource.getContent(resourceName);
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        return jsExecutor.executeScript(script, args);
     }
 }
