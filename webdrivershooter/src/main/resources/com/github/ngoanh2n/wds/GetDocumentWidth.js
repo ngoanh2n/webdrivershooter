6345d26a3772321f550a7b62ff3160eda14ab640
ngoanh2n <ngoanh2n@gmail.com>
2023-04-20 22:00:17 +0700

    Add JS script: com/github/ngoanh2n/wds/GetDocumentWidth.js

---
+++ webdrivershooter/src/main/resources/com/github/ngoanh2n/wds/GetDocumentWidth.js
@@ -0,0 +1,7 @@
+return Math.max(
+  document.body.scrollWidth,
+  document.body.offsetWidth,
+  document.documentElement.clientWidth,
+  document.documentElement.scrollWidth,
+  document.documentElement.offsetWidth
+);
