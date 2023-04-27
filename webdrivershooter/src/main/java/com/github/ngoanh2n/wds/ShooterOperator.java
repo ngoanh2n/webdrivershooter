87187f3694aea9c1e09194282d4919d068b0d721
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 22:38:06 +0700

    Add ShooterOperator

---
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterOperator.java
@@ -0,0 +1,7 @@
+package com.github.ngoanh2n.wds;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public abstract class ShooterOperator<Options extends ShooterOptions> {
+}
