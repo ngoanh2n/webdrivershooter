97523267142fc7e830241bf2dc614f826538c5b8
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 21:57:09 +0700

    Add Screener.getElementRectTop()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screener.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screener.java
@@ -127,4 +127,9 @@ public class Screener {
         Object value = executeScript(driver, "com/github/ngoanh2n/wds/GetElementRectLeft.js", element);
         return (int) (Double.parseDouble(value.toString()) * dpr);
     }
+
+    public int getElementRectTop(WebElement element) {
+        Object value = executeScript(driver, "com/github/ngoanh2n/wds/GetElementRectTop.js", element);
+        return (int) (Double.parseDouble(value.toString()) * dpr);
+    }
 }
