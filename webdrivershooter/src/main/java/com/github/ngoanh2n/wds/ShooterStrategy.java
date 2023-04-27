0f9b71f4a77242ba343ddf25d6b8057b1e8f19e9
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 22:51:43 +0700

    Add ShooterStrategy.verticalScroll()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
@@ -4,4 +4,6 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class ShooterStrategy<Options extends ShooterOptions, Operator extends ShooterOperator<Options>> {
     protected abstract Operator viewport(Options options, WebDriver driver);
+
+    protected abstract Operator verticalScroll(Options options, WebDriver driver);
 }
