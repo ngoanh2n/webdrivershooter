7a009e8f469396fc7f76c77b0a2e13ad01fbd071
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 22:44:58 +0700

    Add ShooterOperator.createScreenshot()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterOperator.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterOperator.java
@@ -1,10 +1,15 @@
 package com.github.ngoanh2n.wds;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 @ParametersAreNonnullByDefault
 public abstract class ShooterOperator<Options extends ShooterOptions> {
@@ -14,4 +19,23 @@ public abstract class ShooterOperator<Options extends ShooterOptions> {
     protected Screenshot screenshot;
     protected BufferedImage image;
     protected Graphics2D graphics;
+
+    //-------------------------------------------------------------------------------//
+
+    private Screenshot createScreenshot() {
+        List<Screengle> results = new ArrayList<>();
+        if (options.ignoredElement() != null) {
+            WebElement element = options.ignoredElement();
+            int x = (int) (element.getLocation().getX() * screener.getDPR());
+            int y = (int) (element.getLocation().getY() * screener.getDPR());
+            int w = (int) (element.getSize().getWidth() * screener.getDPR());
+            int h = (int) (element.getSize().getHeight() * screener.getDPR());
+
+            Point location = new Point(x, y);
+            Dimension size = new Dimension(w, h);
+            results.add(Screengle.from(location, size));
+        }
+        Color color = options.decoratedColor();
+        return new Screenshot(color, results.toArray(new Screengle[]{}));
+    }
 }
