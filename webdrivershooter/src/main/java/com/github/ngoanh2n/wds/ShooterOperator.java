61aef863cdf3c173493c3f5888452d0b4b968460
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 22:39:16 +0700

    Add fields for ShooterOperator

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterOperator.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterOperator.java
@@ -1,7 +1,17 @@
 package com.github.ngoanh2n.wds;
 
+import org.openqa.selenium.WebDriver;
+
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 @ParametersAreNonnullByDefault
 public abstract class ShooterOperator<Options extends ShooterOptions> {
+    protected Options options;
+    protected WebDriver driver;
+    protected Screener screener;
+    protected Screenshot screenshot;
+    protected BufferedImage image;
+    protected Graphics2D graphics;
 }
