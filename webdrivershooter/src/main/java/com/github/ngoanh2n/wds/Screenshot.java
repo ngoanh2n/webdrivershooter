3c1090460ee91a46e7bcfd8455397c02a047c923
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 22:33:14 +0700

    Add fields and constructor

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screenshot.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screenshot.java
@@ -1,11 +1,24 @@
 package com.github.ngoanh2n.wds;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Screenshot {
+    private final Color decoratedColor;
+    private final Screengle[] ignoredElements;
     private BufferedImage image;
+    private BufferedImage decoratedImage;
 
-    public Screenshot(BufferedImage image) {
+    public Screenshot(Color decoratedColor, Screengle[] ignoredElements) {
+        this.decoratedColor = decoratedColor;
+        this.ignoredElements = ignoredElements;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
         this.image = image;
     }
 }
