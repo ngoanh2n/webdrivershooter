8db83cc1b5a53969c3538684525dad5b0a269cd3
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 22:51:25 +0700

    Add ShooterStrategy.viewport()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
@@ -1,4 +1,7 @@
 package com.github.ngoanh2n.wds;
 
+import org.openqa.selenium.WebDriver;
+
 public abstract class ShooterStrategy<Options extends ShooterOptions, Operator extends ShooterOperator<Options>> {
+    protected abstract Operator viewport(Options options, WebDriver driver);
 }
