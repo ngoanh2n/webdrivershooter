f33b8dfed762e46e06bf037375ee6d5ce158f5a0
ngoanh2n <ngoanh2n@gmail.com>
2023-04-27 21:47:33 +0700

    Define fields and constructor

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screengle.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screengle.java
@@ -1,4 +1,17 @@
 package com.github.ngoanh2n.wds;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
 public class Screengle {
+    private final Point location;
+    private final Dimension size;
+
+    public Screengle(Point location, Dimension size) {
+        this.location = location;
+        this.size = size;
+    }
 }
