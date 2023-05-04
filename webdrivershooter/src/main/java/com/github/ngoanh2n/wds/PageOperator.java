c8827c56de0432789c64dff26c7909e4c460b571
ngoanh2n <ngoanh2n@gmail.com>
2023-05-04 22:35:07 +0700

    Add PageOperator.scrollSY()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/PageOperator.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/PageOperator.java
@@ -1,9 +1,18 @@
 package com.github.ngoanh2n.wds;
 
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 
 public abstract class PageOperator extends ShooterOperator<PageOptions> {
     protected PageOperator(PageOptions options, WebDriver driver) {
         super(options, driver);
+    }
+
+    //-------------------------------------------------------------------------------//
+
+    protected void scrollSY(int multiplierY) {
+        int pointX = screener.getDocumentScrollX();
+        int pointY = screener.getInnerRect().getHeight() * multiplierY;
+        screener.scrollToPoint(new Point(pointX, pointY));
     }
 }
