4cd94bce19d411ee5ba47c5509bab63360c9c502
ngoanh2n <ngoanh2n@gmail.com>
2023-05-04 23:29:54 +0700

    Rename ShooterStrategy methods

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
@@ -3,11 +3,11 @@ package com.github.ngoanh2n.wds;
 import org.openqa.selenium.WebDriver;
 
 public abstract class ShooterStrategy<Options extends ShooterOptions, Operator extends ShooterOperator<Options>> {
-    protected abstract Operator viewport(Options options, WebDriver driver);
+    protected abstract Operator byScroll0(Options options, WebDriver driver);
 
-    protected abstract Operator verticalScroll(Options options, WebDriver driver);
+    protected abstract Operator byScrollY(Options options, WebDriver driver);
 
-    protected abstract Operator horizontalScroll(Options options, WebDriver driver);
+    protected abstract Operator byScrollX(Options options, WebDriver driver);
 
-    protected abstract Operator bothDirectionScroll(Options options, WebDriver driver);
+    protected abstract Operator byScrollXY(Options options, WebDriver driver);
 }
