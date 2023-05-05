37948d5e77b511deb7ed12dfd776506d39de6bc6
ngoanh2n <ngoanh2n@gmail.com>
2023-05-05 21:44:56 +0700

    Implement WebDriverShooter.shootScrollY()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ElementShooter.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ElementShooter.java
@@ -28,7 +28,22 @@ class ElementShooter extends WebDriverShooter<ElementOptions, ElementOperator> {
 
     @Override
     protected ElementOperator byScrollY(ElementOptions options, WebDriver driver) {
-        return null;
+        return new ElementOperator(options, driver) {
+            @Override
+            protected int imageWidth() {
+                return screener.getInnerRect().getWidth();
+            }
+
+            @Override
+            protected int imageHeight() {
+                return screener.getOuterRect().getHeight();
+            }
+
+            @Override
+            protected boolean imageFull(@Nonnull BufferedImage part) {
+                return imageHeight() == part.getHeight(null);
+            }
+        };
     }
 
     @Override
@@ -59,7 +74,22 @@ class ElementShooter extends WebDriverShooter<ElementOptions, ElementOperator> {
 
     @Override
     protected Screenshot shootScrollY(ElementOptions options, ElementOperator operator, WebDriver driver) {
-        return null;
+        int partsY = operator.getPartsY();
+
+        for (int partY = 0; partY < partsY; partY++) {
+            operator.scrollSY(partY);
+            operator.sleep();
+
+            Screenshot screenshot = page(driver);
+            BufferedImage part = screenshot.getImage();
+            operator.mergePart0S(part);
+
+            if (operator.imageFull(part)) {
+                operator.getGraphics().dispose();
+                break;
+            }
+        }
+        return operator.getScreenshot();
     }
 
     @Override
