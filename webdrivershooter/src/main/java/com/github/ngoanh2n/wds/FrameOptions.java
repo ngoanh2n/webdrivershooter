b24176da4db9148efaa1cc2e66acfce0f302830c
ngoanh2n <ngoanh2n@gmail.com>
2023-05-05 21:50:37 +0700

    Add FrameOptions.Builder

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/FrameOptions.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/FrameOptions.java
@@ -2,6 +2,68 @@ package com.github.ngoanh2n.wds;
 
 import org.openqa.selenium.WebElement;
 
+import java.awt.*;
+
 public interface FrameOptions extends PageOptions {
+    static Builder builder() {
+        return new FrameOptions.Builder();
+    }
+
+    static FrameOptions defaults() {
+        return builder().build();
+    }
+
+    //-------------------------------------------------------------------------------//
+
     WebElement frame();
+
+    //===============================================================================//
+
+    class Builder extends ShooterOptions.Builder<Builder> {
+        private WebElement frame;
+
+        private Builder() {
+            super();
+            this.frame = null;
+        }
+
+        public Builder setFrame(WebElement frame) {
+            this.frame = frame;
+            return this;
+        }
+
+        public FrameOptions build() {
+            return new FrameOptions() {
+                @Override
+                public WebElement frame() {
+                    return frame;
+                }
+
+                @Override
+                public int scrollDelay() {
+                    return scrollDelay;
+                }
+
+                @Override
+                public int shooterStrategy() {
+                    return shooterStrategy;
+                }
+
+                @Override
+                public boolean checkDPR() {
+                    return checkDevicePixelRatio;
+                }
+
+                @Override
+                public Color decoratedColor() {
+                    return decoratedColor;
+                }
+
+                @Override
+                public WebElement ignoredElement() {
+                    return ignoredElement;
+                }
+            };
+        }
+    }
 }
