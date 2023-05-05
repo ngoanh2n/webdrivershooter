3b9ced617b01a703ba7feae56d15b499d8ad732c
ngoanh2n <ngoanh2n@gmail.com>
2023-05-05 21:38:47 +0700

    Add ElementOptions.Builder

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ElementOptions.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ElementOptions.java
@@ -2,6 +2,68 @@ package com.github.ngoanh2n.wds;
 
 import org.openqa.selenium.WebElement;
 
+import java.awt.*;
+
 public interface ElementOptions extends ShooterOptions {
+    static Builder builder() {
+        return new ElementOptions.Builder();
+    }
+
+    static ElementOptions defaults() {
+        return builder().build();
+    }
+
+    //-------------------------------------------------------------------------------//
+
     WebElement element();
+
+    //===============================================================================//
+
+    class Builder extends ShooterOptions.Builder<Builder> {
+        private WebElement element;
+
+        private Builder() {
+            super();
+            this.element = null;
+        }
+
+        public Builder setElement(WebElement element) {
+            this.element = element;
+            return this;
+        }
+
+        public ElementOptions build() {
+            return new ElementOptions() {
+                @Override
+                public WebElement element() {
+                    return element;
+                }
+
+                @Override
+                public int scrollDelay() {
+                    return scrollDelay;
+                }
+
+                @Override
+                public int shooterStrategy() {
+                    return shooterStrategy;
+                }
+
+                @Override
+                public boolean checkDPR() {
+                    return checkDevicePixelRatio;
+                }
+
+                @Override
+                public Color decoratedColor() {
+                    return decoratedColor;
+                }
+
+                @Override
+                public WebElement ignoredElement() {
+                    return ignoredElement;
+                }
+            };
+        }
+    }
 }
