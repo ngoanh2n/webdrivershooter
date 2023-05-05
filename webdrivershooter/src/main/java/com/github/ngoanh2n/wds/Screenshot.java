e3d29e0e8c35c11aa1e40102a9f84e968465a979
ngoanh2n <ngoanh2n@gmail.com>
2023-05-05 22:19:45 +0700

    Modify constructor for Screenshot

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screenshot.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Screenshot.java
@@ -4,21 +4,19 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Screenshot {
+    private final BufferedImage image;
     private final Color decoratedColor;
-    private final Screengle[] ignoredElements;
-    private BufferedImage image;
-    private BufferedImage decoratedImage;
+    private final Screengle[] decoratedScreengles;
 
-    public Screenshot(Color decoratedColor, Screengle[] ignoredElements) {
+    public Screenshot(BufferedImage image, Color decoratedColor, Screengle[] decoratedScreengles) {
+        this.image = image;
         this.decoratedColor = decoratedColor;
-        this.ignoredElements = ignoredElements;
+        this.decoratedScreengles = decoratedScreengles;
     }
+
+    //-------------------------------------------------------------------------------//
 
     public BufferedImage getImage() {
         return image;
-    }
-
-    public void setImage(BufferedImage image) {
-        this.image = image;
     }
 }
