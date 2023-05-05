260a7a8880630ed8e3bfa96a3ae06632fe37aa2f
ngoanh2n <ngoanh2n@gmail.com>
2023-05-05 21:51:31 +0700

    Add constructor

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/FrameOperator.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/FrameOperator.java
@@ -1,12 +1,23 @@
 package com.github.ngoanh2n.wds;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 public abstract class FrameOperator extends PageOperator {
-    protected FrameOperator(PageOptions options, WebDriver driver) {
+    protected WebElement frame;
+    protected Screener framer;
+
+    protected FrameOperator(FrameOptions options, WebDriver driver) {
         super(options, driver);
+        this.frame = options.frame();
+        this.screener.scrollElementIntoView(frame);
+
+        boolean checkDPR = options.checkDPR();
+        this.framer = Screener.element(checkDPR, driver, frame);
+        this.driver.switchTo().frame(frame);
+        this.screener = Screener.page(checkDPR, driver);
     }
 }
