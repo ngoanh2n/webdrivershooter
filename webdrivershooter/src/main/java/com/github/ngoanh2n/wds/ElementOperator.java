d7db4061ca6bc9fc65ed71b592a39f119bcd284e
ngoanh2n <ngoanh2n@gmail.com>
2023-05-05 21:40:35 +0700

    Add constructor

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ElementOperator.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ElementOperator.java
@@ -1,9 +1,18 @@
 package com.github.ngoanh2n.wds;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
 public abstract class ElementOperator extends ShooterOperator<ElementOptions> {
+    protected WebElement element;
+
     protected ElementOperator(ElementOptions options, WebDriver driver) {
         super(options, driver);
+        this.element = options.element();
+        this.screener = Screener.element(options.checkDPR(), driver, element);
+        this.screener.scrollElementIntoView(element);
     }
 }
