dd05c51f5f86ae66af2ec7ac82a31cc58b0d5fff
ngoanh2n <ngoanh2n@gmail.com>
2023-05-05 21:56:29 +0700

    Override PageShooter.byScroll0()

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/FrameShooter.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/FrameShooter.java
@@ -1,4 +1,28 @@
 package com.github.ngoanh2n.wds;
 
+import org.openqa.selenium.WebDriver;
+
+import javax.annotation.Nonnull;
+import java.awt.image.BufferedImage;
+
 class FrameShooter extends PageShooter {
+    @Override
+    protected PageOperator byScroll0(PageOptions options, WebDriver driver) {
+        return new FrameOperator((FrameOptions) options, driver) {
+            @Override
+            protected int imageWidth() {
+                return framer.getInnerRect().getWidth();
+            }
+
+            @Override
+            protected int imageHeight() {
+                return framer.getInnerRect().getHeight();
+            }
+
+            @Override
+            protected boolean imageFull(@Nonnull BufferedImage part) {
+                return true;
+            }
+        };
+    }
 }
