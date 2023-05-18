959ad125cb05495d38a52bf6f2d3cf490ec6514e
ngoanh2n <ngoanh2n@gmail.com>
2023-05-18 21:41:16 +0700

    Add Javadoc for ShooterStrategy

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterStrategy.java
@@ -2,7 +2,20 @@ package com.github.ngoanh2n.wds;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Strategies to take screenshot.
+ *
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ */
 public interface ShooterStrategy<Operator extends ShooterOperator> {
+    /**
+     * Take screenshot.
+     *
+     * @param options  {@link ShooterOptions} to adjust behaviors of {@link WebDriverShooter}.
+     * @param driver   The current {@link WebDriver}.
+     * @param operator The helper to operate on screen.
+     * @return The {@link Screenshot}.
+     */
     default Screenshot shoot(ShooterOptions options, WebDriver driver, Operator operator) {
         switch (options.shooter()) {
             case 1:
@@ -16,11 +29,43 @@ public interface ShooterStrategy<Operator extends ShooterOperator> {
         }
     }
 
+    /**
+     * Take screenshot of viewport.
+     *
+     * @param options  {@link ShooterOptions} to adjust behaviors of {@link WebDriverShooter}.
+     * @param driver   The current {@link WebDriver}.
+     * @param operator The helper to operate on screen.
+     * @return The {@link Screenshot}.
+     */
     Screenshot shootViewport(ShooterOptions options, WebDriver driver, Operator operator);
 
+    /**
+     * Take screenshot while scrolling vertically.
+     *
+     * @param options  {@link ShooterOptions} to adjust behaviors of {@link WebDriverShooter}.
+     * @param driver   The current {@link WebDriver}.
+     * @param operator The helper to operate on screen.
+     * @return The {@link Screenshot}.
+     */
     Screenshot shootVerticalScroll(ShooterOptions options, WebDriver driver, Operator operator);
 
+    /**
+     * Take screenshot while scrolling horizontally.
+     *
+     * @param options  {@link ShooterOptions} to adjust behaviors of {@link WebDriverShooter}.
+     * @param driver   The current {@link WebDriver}.
+     * @param operator The helper to operate on screen.
+     * @return The {@link Screenshot}.
+     */
     Screenshot shootHorizontalScroll(ShooterOptions options, WebDriver driver, Operator operator);
 
+    /**
+     * Take screenshot while scrolling vertically and horizontally.
+     *
+     * @param options  {@link ShooterOptions} to adjust behaviors of {@link WebDriverShooter}.
+     * @param driver   The current {@link WebDriver}.
+     * @param operator The helper to operate on screen.
+     * @return The {@link Screenshot}.
+     */
     Screenshot shootFullScroll(ShooterOptions options, WebDriver driver, Operator operator);
 }
