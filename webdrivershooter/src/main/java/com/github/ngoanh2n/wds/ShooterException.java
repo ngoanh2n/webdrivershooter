0f3545ad90acbcd064264d99badc378664dab983
ngoanh2n <ngoanh2n@gmail.com>
2023-05-19 20:54:33 +0700

    Add Javadoc for ShooterException

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterException.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ShooterException.java
@@ -2,15 +2,29 @@ package com.github.ngoanh2n.wds;
 
 import com.github.ngoanh2n.RuntimeError;
 
+/**
+ * Runtime exception for WebDriverShooter.
+ *
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ */
 public class ShooterException extends RuntimeError {
+    /**
+     * {@inheritDoc}
+     */
     public ShooterException(String message) {
         super(message);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public ShooterException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public ShooterException(String message, Throwable cause) {
         super(message, cause);
     }
