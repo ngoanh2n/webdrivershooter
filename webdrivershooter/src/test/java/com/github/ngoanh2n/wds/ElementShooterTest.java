1b60f7243e34d3912f44019592d7ee9cbf591ff0
ngoanh2n <ngoanh2n@gmail.com>
2023-05-25 22:52:44 +0700

    Add ElementShooterTest

---
+++ webdrivershooter/src/test/java/com/github/ngoanh2n/wds/ElementShooterTest.java
@@ -0,0 +1,55 @@
+package com.github.ngoanh2n.wds;
+
+import com.github.ngoanh2n.EnabledIfProperty;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@EnabledIfProperty(name = "wds.browser", value = {"chrome", "safari", "firefox", "edge"})
+public class ElementShooterTest extends AbstractSeleniumTest {
+    protected WebElement element;
+    protected WebElement ignore1;
+    protected WebElement ignore2;
+
+    @Override
+    @BeforeEach
+    protected void openDriver(TestInfo testInfo) {
+        super.openDriver(testInfo);
+        driver.get("https://mvnrepository.com");
+        element = driver.findElement(By.cssSelector("div[class='content']"));
+        ignore1 = driver.findElement(By.xpath("//div[@class='posts']/div[1]//h2"));
+        ignore2 = driver.findElement(By.xpath("//div[@class='posts']/div[2]//h2"));
+    }
+
+    @Test
+    @Order(1)
+    void viewport() {
+        ShooterOptions options = ShooterOptions.builder().shootViewport().maskElements(ignore1, ignore2).build();
+        screenshot = WebDriverShooter.element(options, element, driver);
+    }
+
+    @Test
+    @Order(2)
+    void verticalScroll() {
+        ShooterOptions options = ShooterOptions.builder().shootVerticalScroll().build();
+        screenshot = WebDriverShooter.element(options, element, driver);
+    }
+
+    @Test
+    @Order(3)
+    void horizontalScroll() {
+        ShooterOptions options = ShooterOptions.builder().shootHorizontalScroll().maskElements(ignore1, ignore2).build();
+        screenshot = WebDriverShooter.element(options, element, driver);
+    }
+
+    @Test
+    @Order(4)
+    void fullScroll() {
+        ShooterOptions options = ShooterOptions.builder().shootFullScroll().maskElements(ignore1, ignore2).build();
+        screenshot = WebDriverShooter.element(options, element, driver);
+    }
+}
