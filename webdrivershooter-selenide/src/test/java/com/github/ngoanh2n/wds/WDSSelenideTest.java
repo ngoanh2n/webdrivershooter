b747b095965caaacb118889eb65113ddd63425ae
ngoanh2n <ngoanh2n@gmail.com>
2023-05-27 13:50:54 +0700

    Add tests for WDSSelenide

---
+++ webdrivershooter-selenide/src/test/java/com/github/ngoanh2n/wds/WDSSelenideTest.java
@@ -0,0 +1,37 @@
+package com.github.ngoanh2n.wds;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ */
+public class WDSSelenideTest {
+    @BeforeAll
+    static void beforeAll() {
+        Assertions.assertThrows(ShooterException.class, WebDriverShooter::getDriver);
+        createWebDriver();
+        Assertions.assertNotNull(WebDriverShooter.getDriver());
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        Assertions.assertNotNull(WebDriverShooter.getDriver());
+    }
+
+    @Test
+    void test() {
+        Assertions.assertNotNull(WebDriverShooter.getDriver());
+    }
+
+    private static void createWebDriver() {
+        Configuration.timeout = 10 * 1000;
+        Configuration.pollingInterval = 500;
+        Configuration.browser = "chrome";
+        Selenide.open();
+    }
+}
