aea1d80618fb59a224c4b283b9f8d69a38ad2fea
ngoanh2n <ngoanh2n@gmail.com>
2023-12-02 12:56:47 +0700

    [javadoc] Add for Rectangle

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Rectangle.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Rectangle.java
@@ -1,48 +1,105 @@
 package com.github.ngoanh2n.wds;
 
+/**
+ * A copy of java.awt.Rectangle, avoid to load dependency on Java AWT.<br><br>
+ *
+ * <em>Repository:</em>
+ * <ul>
+ *     <li><em>GitHub: <a href="https://github.com/ngoanh2n/webdrivershooter">ngoanh2n/webdrivershooter</a></em></li>
+ *     <li><em>Maven: <a href="https://mvnrepository.com/artifact/com.github.ngoanh2n/webdrivershooter">com.github.ngoanh2n:webdrivershooter</a></em></li>
+ * </ul>
+ *
+ * @author ngoanh2n
+ * @since 2021
+ */
 public class Rectangle {
-    private Position position;
-    private Dimension dimension;
+    private Position p;
+    private Dimension d;
 
     public Rectangle(int x, int y, int w, int h) {
-        this.position = new Position(x, y);
-        this.dimension = new Dimension(w, h);
+        this.p = new Position(x, y);
+        this.d = new Dimension(w, h);
     }
 
+    /**
+     * Get the X coordinate of this {@code Rectangle}.
+     *
+     * @return The X coordinate.
+     */
     public int getX() {
-        return position.getX();
+        return p.getX();
     }
 
+    /**
+     * Get the Y coordinate of this {@code Rectangle}.
+     *
+     * @return The Y coordinate.
+     */
     public int getY() {
-        return position.getY();
+        return p.getY();
     }
 
+    /**
+     * Get width of this {@code Rectangle}.
+     *
+     * @return The width of this {@code Rectangle}.
+     */
     public int getWidth() {
-        return dimension.getWidth();
+        return d.getWidth();
     }
 
+    /**
+     * Get height of this {@code Rectangle}.
+     *
+     * @return The height of this {@code Rectangle}.
+     */
     public int getHeight() {
-        return dimension.getHeight();
+        return d.getHeight();
     }
 
+    /**
+     * Get position of this {@code Rectangle}.
+     *
+     * @return The position of this {@code Rectangle}.
+     */
     public Position getPosition() {
-        return position;
+        return p;
     }
 
+    /**
+     * Set position for this {@code Rectangle}.
+     *
+     * @param p The new position for this {@code Rectangle}.
+     */
+    public void setPosition(Position p) {
+        this.p = p;
+    }
+
+    /**
+     * Get dimension of this {@code Rectangle}.
+     *
+     * @return The dimension of this {@code Rectangle}.
+     */
     public Dimension getDimension() {
-        return dimension;
+        return d;
     }
 
-    public void setPosition(Position position) {
-        this.position = position;
+    /**
+     * Set dimension for this {@code Rectangle}.
+     *
+     * @param d The new dimension for this {@code Rectangle}.
+     */
+    public void setDimension(Dimension d) {
+        this.d = d;
     }
 
-    public void setDimension(Dimension dimension) {
-        this.dimension = dimension;
-    }
-
+    /**
+     * Returns a string representation of this {@code Rectangle}.
+     *
+     * @return A string representation of this {@code Rectangle}.
+     */
     @Override
     public String toString() {
-        return String.format("%s %s", position.toString(), dimension.toString());
+        return String.format("%s %s", p, d);
     }
 }
