6bf2f429c0460588e40004ff22d949a89a67c9c1
ngoanh2n <ngoanh2n@gmail.com>
2023-12-02 10:33:04 +0700

    [wds] Add ImageUtils

---
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/ImageUtils.java
@@ -0,0 +1,17 @@
+package com.github.ngoanh2n.wds;
+
+/**
+ * Utils for image process.<br><br>
+ *
+ * <em>Repository:</em>
+ * <ul>
+ *     <li><em>GitHub: <a href="https://github.com/ngoanh2n/webdrivershooter">ngoanh2n/webdrivershooter</a></em></li>
+ *     <li><em>Maven: <a href="https://mvnrepository.com/artifact/com.github.ngoanh2n/webdrivershooter">com.github.ngoanh2n:webdrivershooter</a></em></li>
+ * </ul>
+ *
+ * @author ngoanh2n
+ * @since 2021
+ */
+public class ImageUtils {
+
+}
