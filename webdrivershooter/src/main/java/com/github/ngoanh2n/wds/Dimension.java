9e0555bec462d1db5d2628c4f9707127f9140f05
ngoanh2n <ngoanh2n@gmail.com>
2023-12-30 13:33:58 +0700

    [wds] Add methods to increase and decrease w:h

--- webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Dimension.java
+++ webdrivershooter/src/main/java/com/github/ngoanh2n/wds/Dimension.java
@@ -1,7 +1,11 @@
 package com.github.ngoanh2n.wds;
 
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
 /**
- * A copy of java.awt.Dimension, avoid to load dependency on Java AWT.<br><br>
+ * A copy of java.awt.Dimension.<br><br>
  *
  * <em>Repository:</em>
  * <ul>
@@ -12,6 +16,8 @@ package com.github.ngoanh2n.wds;
  * @author ngoanh2n
  * @since 2021
  */
+@CanIgnoreReturnValue
+@ParametersAreNonnullByDefault
 public class Dimension {
     private int w;
     private int h;
@@ -23,6 +29,15 @@ public class Dimension {
         this.w = w;
         this.h = h;
     }
+
+    /**
+     * Construct a new {@link Dimension} by other {@link Dimension}.
+     */
+    public Dimension(Dimension other) {
+        this(other.w, other.h);
+    }
+
+    //-------------------------------------------------------------------------------//
 
     /**
      * Get width of this {@code Dimension}.
@@ -58,6 +73,52 @@ public class Dimension {
      */
     public void setHeight(int h) {
         this.h = h;
+    }
+
+    /**
+     * Increase width for this {@code Dimension} by {@code value}.
+     *
+     * @param value The value to be added to width of this {@code Dimension}.
+     */
+    public void incW(int value) {
+        w += value;
+    }
+
+    /**
+     * Decrease width for this {@code Dimension} by {@code value}.
+     *
+     * @param value The value to be subtracted to width of this {@code Dimension}.
+     */
+    public void decW(int value) {
+        w -= value;
+    }
+
+    /**
+     * Increase height for this {@code Dimension} by {@code value}.
+     *
+     * @param value The value to be added to height of this {@code Dimension}.
+     */
+    public void incH(int value) {
+        h += value;
+    }
+
+    /**
+     * Decrease height for this {@code Dimension} by {@code value}.
+     *
+     * @param value The value to be subtracted to height of this {@code Dimension}.
+     */
+    public void decH(int value) {
+        h -= value;
+    }
+
+    /**
+     * Check whether the current {@code Dimension} equals to other {@code Dimension}.
+     *
+     * @param other {@code Dimension} to be compared.
+     * @return {@code true} if the current {@code Dimension} equals to other {@code Dimension}; {@code false} otherwise.
+     */
+    public boolean equals(Dimension other) {
+        return w == other.w && h == other.h;
     }
 
     /**
