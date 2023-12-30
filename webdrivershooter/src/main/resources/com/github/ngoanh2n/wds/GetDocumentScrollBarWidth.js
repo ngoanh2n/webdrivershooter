8e665d67888fdc681ed50c43dd1ad566715e2ae9
ngoanh2n <ngoanh2n@gmail.com>
2023-12-30 13:38:39 +0700

    [wds] Improve GetDocumentScrollBarWidth.js

--- webdrivershooter/src/main/resources/com/github/ngoanh2n/wds/GetDocumentScrollBarWidth.js
+++ webdrivershooter/src/main/resources/com/github/ngoanh2n/wds/GetDocumentScrollBarWidth.js
@@ -1,4 +1,35 @@
-return Math.max(
+var width = Math.max(
+    0,
     window.innerWidth - document.body.clientWidth,
-    document.body.offsetWidth - document.body.clientWidth
+    document.body.offsetWidth - document.body.clientWidth,
 );
+
+if (width == 0) {
+    const docH = Math.max(
+        document.body.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight
+    );
+    const viewportH = window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+    const hasScroll = document.body.scrollHeight > document.body.clientHeight;
+
+    if (hasScroll || (docH > viewportH)) {
+        const outer = document.createElement('div');
+        outer.style.visibility = 'hidden';
+        outer.style.overflow = 'scroll';
+        outer.style.msOverflowStyle = 'scrollbar';
+
+        const inner = document.createElement('div');
+        outer.appendChild(inner);
+        document.body.appendChild(outer);
+
+        width = (outer.offsetWidth - inner.offsetWidth);
+        outer.parentNode.removeChild(outer);
+    }
+}
+
+return Math.max(0, width);
